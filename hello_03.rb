f7bdb16cea5d139e3e98554a08759202d2cf46c1
Naoyuki Hirata <naoyuki.hirata.biz@gmail.com>
2016-09-07 15:45:41 +0000

    First commit

---
+++ hello_03.rb
@@ -0,0 +1,17 @@
+# 変数
+# - 英小文字 _
+
+# msg = "hello world"
+# puts msg
+
+# msg = "hello world again"
+# puts msg
+
+# 定数
+# - 英大文字
+
+VERSION = 1.1
+puts VERSION
+
+VERSION = 1.2
+puts VERSION
---
+++ hello_05.rb
@@ -0,0 +1,20 @@
+# 数値
+# 32 4.8
+
+# p 4.8.class
+# p 4.8.methods
+
+# + - * / % **
+
+p 10 + 3
+p 10 * 3
+p 2.4 * 2
+p 10 / 3 # 3
+p 10 % 3 # 1
+p 10.0 / 3
+p Rational(2, 5) + Rational(3, 4)
+p 2/5r + 3/4r
+
+p 52.6.round
+p 52.6.floor
+p 52.6.ceil
